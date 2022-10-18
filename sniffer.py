b0f5985ea0da9fc966202bdffde7424ed00fc4ff
sanghun lee <tkdgn821@naver.com>
2022-10-19 00:55:52 +0900

    ver. 19 Oct. 2022

--- sniffer.py
+++ sniffer.py
@@ -1,15 +1,21 @@
 import os
 from scapy.all import *
 def showPacket(packet):
-    a = packet.show()
-    print(a)
+    #a = packet.show()
+    #print(a)
+    packet.display()
     print('#####################################################')
 
 def sniffing(filter):
     sniff(filter = filter, prn = showPacket, count = 10)
 
 if __name__ == '__main__':
-    filter = 'tcp or udp'  # can use protocol list == ['icmp', 'arp', 'rarp', 'tcp', 'udp'] and also can use 'or' sign / if filter == none, it can detact llc(ieee 802.3)
+    filter = None
+  # can use protocol list == ['icmp', 'arp', 'rarp', 'tcp', 'udp'] and also can use 'or' sign / if filter == none, it can detact llc(ieee 802.3)
     sniffing(filter)
     
 os.system('pause')
+
+
+
+
